da66a7fa31df9ee8d2303e5075db6021924230c6
v_yoongliu <v_yoongliu@tencent.com>
2021-10-11 09:53:36 +0800

    fix: 初次编译

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main (String [] args) {
+        System.out.println("入口启动");
+        Server.Server();
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,5 @@
+public class Server {
+    public static void Server() {
+        System.out.println("Server启动");
+    }
+}
